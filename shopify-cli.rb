065eafb44d14f4183ce2fe7759778e021b179132
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2020-05-18 17:54:32 -0400

    Updated sha256 (suite of files attempt)

--- shopify-cli.rb
+++ shopify-cli.rb
@@ -4,7 +4,7 @@ class ShopifyCli < Formula
   desc "Shopify App CLI helps you build Shopify apps faster"
   homepage "https://github.com/mkevinosullivan/shopify-app-cli"
   url "https://github.com/mkevinosullivan/shopify-app-cli/blob/convert_to_gem/archive/shopify-cli-0.0.2.tar.gz"
-  sha256 "056f2c8336a7d1edfc1b1896979f2f99aa1ba6eea13f06339cb9c89a504a8448"
+  sha256 "876e087d510a6c238119a5667367b7284aa1070d7df32648b40df9abcfff79e1"
   version "0.0.2"
 
   depends_on "ruby"
@@ -12,8 +12,8 @@ class ShopifyCli < Formula
   bottle :unneeded
 
   def install
-    system('gem', 'install', 'shopify-app-cli')
-    # libexec.install Dir["*"]
-    # bin.write_exec_script (libexec/"shopify-cli")
+    # system('gem', 'install', 'shopify-app-cli')
+    libexec.install Dir["*"]
+    bin.write_exec_script (libexec/"exe/shopify-cli")
   end
 end
