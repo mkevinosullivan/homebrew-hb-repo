0164f02faccfd230150f04f8d30b55a67a8d64f4
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2020-05-18 18:00:29 -0400

    Updated sha256

--- shopify-cli.rb
+++ shopify-cli.rb
@@ -4,7 +4,7 @@ class ShopifyCli < Formula
   desc "Shopify App CLI helps you build Shopify apps faster"
   homepage "https://github.com/mkevinosullivan/shopify-app-cli"
   url "https://github.com/mkevinosullivan/shopify-app-cli/blob/convert_to_gem/archive/shopify-cli-0.0.2.tar.gz"
-  sha256 "876e087d510a6c238119a5667367b7284aa1070d7df32648b40df9abcfff79e1"
+  sha256 "9d992545484e79cbf099e1824eca546de3ab8e942b3b2b33cbe3d2e6de0eb9c4"
   version "0.0.2"
 
   depends_on "ruby"
